be4be0b73ec3cd1f5e408d5a2bd4376f2291e03f
Joe Bruno <joevbruno@me.com>
2015-12-20 16:06:16 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var coreUI = {
+  registerComponent: function(name, src) {
+    if (coreUI[name]) {
+      throw new Error('Component ' + name + ' already exists in coreUI. Use a different name.');
+    }
+    coreUI[name] = src;
+    return coreUI;
+  },
+  registerComponents: function(obj) {
+    Object.keys(obj).map(function(component) {
+      coreUI.registerComponent(component, object[component]);
+    });
+  },
+  getComponents: function() {
+    var components = coreUI;
+    delete components.registerComponent;
+    delete components.registerComponents;
+    delete components.getComponents;
+    return components;
+  } 
+};
+module.exports = coreUI; 
+
+
